4ef9fce926a9a2e6aa60a396fe20f26c132bc952
Ankit Arora <ankit992@live.in>
2017-10-12 00:14:44 +0530

    adding header component to the application

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,15 @@
+//This file is for both platforms but we'll be developing for android in this one. 
+
+//Import a library to create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
+
+//Create a component
+const App = () => (
+  <Header />
+);
+
+//Render it to the device
 
 AppRegistry.registerComponent('albums', () => App);
