b5ff39d6e5c7b174be4d80567dfc3d16f5d52756
Ankit Arora <ankit992@live.in>
2017-10-12 16:07:54 +0530

    update header component styles

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,9 +1,32 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //make a component
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+const { textStyle, viewStyle } = styles;
+return (
+    <View style={viewStyle}>
+        <Text style={textStyle}>Albums!</Text>
+    </View>
+);
+};
 
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 1,
+        elevation: 5,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
 // make a component available to other parts of the app
 export default Header;
